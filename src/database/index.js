eaa03dcaed75db1732e6f4f94aaa212ad98cefef
Diogo Souza <diogouba11@gmail.com>
2022-01-22 15:51:28 -0300

    update database

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 // Conexão com o banco de dados MongoDB
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/ozmap', { useMongoClient: true}); //Uso de parâmetro
+mongoose.connect('mongodb+srv://dgsouzav:diogo21@cluster0.batqn.mongodb.net/ozmap?retryWrites=true&w=majority')// { useMongoClient: true}); //Uso de parâmetro
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
