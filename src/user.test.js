9eac2e41a689723c0d1b3eb5c259ba6f71406499
Diogo Souza <diogouba11@gmail.com>
2022-01-24 10:53:30 -0300

    testing

---
+++ src/user.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const User = require('./models/user');
+
+describe('Teste do meu server', () => {
+    it('should get main route', async () => {
+        const res = await request('http://localhost:3000').get('/')
+        
+        expect(res.statusCode).toEqual(200)
+        espect(res.body).toHaveProperty('UserSchema')
+    
+    })
+})
